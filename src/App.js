7f5e3850deb974a585f561cd12e15ec872c8a794
Deepakpanghal <deepakpanghal52@gmail.com>
2022-02-13 18:29:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import lata from './lata.jpg';
+
+function App() {
+  return (
+    <div className="App">
+
+      <img src={lata} alt=''/>
+      <br />
+      <h1>Lata Mangeshkar (28 September 1929 – 6 February 2022) </h1>
+      <br />
+      <p>Lata Mangeshkar ([ləˈtaː məŋˈɡeːʃkər] (audio speaker iconlisten)) (born as Hema Mangeshkar; 28 September 1929 – 6 February 2022) was an Indian playback singer and occasional music composer. She is widely considered to have been one of the greatest and most influential singers in India.[7][8] Her contribution to the Indian music industry in a career spanning seven decades gained her honorific titles such as the "Queen of Melody", "Nightingale of India", "Voice of the Millennium".[9]
+
+      Lata Mangeshkar recorded songs in over thirty-six Indian languages and a few foreign languages, though primarily in Hindi, Bengali, Marathi.[9] She also sang in English, Russian, Dutch, and even Swahili. [10] She received several accolades and honors throughout her career. In 1989 the Dadasaheb Phalke Award was bestowed on her by the Government of India.[11] In 2001, in recognition of her contributions to the nation, she was awarded the Bharat Ratna, India's highest civilian honour; she is only the second female singer, after M. S. Subbulakshmi, to receive this honour.[12] France conferred on her its highest civilian award, Officer of the National Order of the Legion of Honour, in 2007.[13]
+
+      She was the recipient of three National Film Awards, 15 Bengal Film Journalists' Association Awards, four Filmfare Best Female Playback Awards, two Filmfare Special Awards, the Filmfare Lifetime Achievement Award and many more. In 1974, she was one of the first Indian playback singers to ever perform at Royal Albert Hall in London, UK. Her last recorded song was "Saugandh Mujhe Is Mitti ki" released on 30 March 2019 as a tribute to the Indian army and Nation.[14] <a href='https://en.wikipedia.org/wiki/Lata_Mangeshkar'>Know More</a> about her.</p>
+
+    </div>
+  );
+}
+
+export default App;
